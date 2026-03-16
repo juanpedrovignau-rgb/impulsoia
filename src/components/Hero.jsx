ea734835f11a0478db2d638e12e2eaa81fe6e51d
Impulso IA <juanpedrovignau@gmail.com>
2026-03-16 19:11:26 -0300

    style: set hero video to assistant pack video and center position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -111,7 +111,6 @@ export default function Hero() {
                             width: '100%',
                             height: '100%',
                             objectFit: 'cover',
-                            objectPosition: 'right center',
                             transform: 'translate(-50%, -50%)',
                             zIndex: 1
                         }}
